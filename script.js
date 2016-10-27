3e33d7b9803fddf4c0ca609f1667a3971668b02b
supertoni <atmartyn@gmail.com>
2016-10-27 22:53:08 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        ////////////////////////////////////  BOUTON HAUT DE PAGE
+            $(function(){
+                $('.btn_up').click(function() {
+                  $('html,body').animate({scrollTop: 0}, 800);
+                });
+            });
+    
